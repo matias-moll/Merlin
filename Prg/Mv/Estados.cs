757d7c326b02344929736221efdb6ad36e75d9e2
mcousido <martincousido@gmail.com>
2013-10-28 17:47:43 -0300

    Primer diseño pantalla de Moviles-Estados

---
+++ Prg/Mv/Estados.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Rivn.Bll;
+using Rivn.Bel;
+using TNGS;
+using TNGS.NetApp;
+using TNGS.NetRoutines;
+using TNGS.NetControls;
+using WeifenLuo.WinFormsUI.Docking;
+
+
+namespace Rivn.Mv
+{
+    public partial class Estados : DockContent
+    {
+        public Estados()
+        {
+            InitializeComponent();
+        }
+
+        private void Estados_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
