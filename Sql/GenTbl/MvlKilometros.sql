7fb4c1aef59f281702b29dccc39b402ce0e59fc6
JuanDesi <juandesimoni@gmail.com>
2013-10-31 15:30:04 -0300

    Revert "arreglo pra rebase"
    This reverts commit fa6b699f117676ff80fd1df6879ab35f3e559c33.

---
+++ Sql/GenTbl/MvlKilometros.sql
@@ -0,0 +1,66 @@
+/*----------------------------------------------------------------------------
+//                 TNG Software Script Generator (Tablas)                     
+//----------------------------------------------------------------------------
+// Fecha              : 28/10/2013 18:04
+// Base de Datos      : TNGS_Rivn
+// Tabla              : MvlKilometros
+//----------------------------------------------------------------------------
+// © 1999-2013 by TNG Software                                      Gndr 5.20
+//---------------------------------------------------------------------------*/
+
+/* ***************************************************************************
+/  Atencion!!!
+/     Este archivo fue generado por un programa en forma automática. Cualquier
+/     modificación que se realize sobre él se perderá en la próxima
+/     regeneración.
+/  **************************************************************************/
+
+/*-------------------------------------------------*/
+/* Selecciono la base en la que se creara la tabla */
+/*-------------------------------------------------*/
+
+use TNGS_Rivn
+go
+
+/*-----------------------------------*/
+/* Crea la tabla en la base indicada */
+/*-----------------------------------*/
+
+print 'Tabla: MvlKilometros'
+
+if exists (select * from sysobjects where id = object_id('dbo.MvlKilometros'))
+begin
+   print '   - Borrando la vieja tabla'
+   drop table dbo.MvlKilometros
+end
+go
+
+print '   - Creando la nueva tabla'
+
+create table MvlKilometros
+   (
+      mkm_ecd_patente tngs_codigo_e,
+      mkm_fyh_fecha tngs_fecyhor,
+      mkm_nro_km tngs_numero,
+      instante tngs_fecyhor,
+      deleted tngs_borrado,
+      usuario tngs_nombre,
+      version tngs_numero,
+      constraint MvlKilometros_pk primary key clustered
+      (
+         mkm_ecd_patente,
+         mkm_fyh_fecha
+      )
+   )
+go
+
+print '   - Asignando permisos a la nueva tabla'
+
+grant select on MvlKilometros to tngsqbe
+
+print ' '
+go
+
+/*--------------------------------------------------------------------------
+// Fin del script de creacion de la tabla: MvlKilometros
+//--------------------------------------------------------------------------*/
