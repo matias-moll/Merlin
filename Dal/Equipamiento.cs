4b36c9daf065aa72b81080fd7d3db3f1a50b9f52
JuanDesi <juandesimoni@gmail.com>
2013-11-03 19:23:01 -0300

    Cambios en Alta Movil, Agregado de la funcion update para el formulario

--- Dal/Equipamiento.cs
+++ Dal/Equipamiento.cs
@@ -10,7 +10,7 @@ namespace Rivn.Dal
     //----------------------------------------------------------------------------
     //                         TNG Software DAL Generator
     //----------------------------------------------------------------------------
-    // Fecha                    : 31/10/2013 16:28
+    // Fecha                    : 03/11/2013 15:54
     // Sistema                  : Rivn
     // Clase para Administrar   : Equipamiento de los móviles
     // Basada en la Tabla       : Equipamiento
--- Dal/MvlKilometros.cs
+++ Dal/MvlKilometros.cs
@@ -10,7 +10,7 @@ namespace Rivn.Dal
     //----------------------------------------------------------------------------
     //                         TNG Software DAL Generator
     //----------------------------------------------------------------------------
-    // Fecha                    : 31/10/2013 16:28
+    // Fecha                    : 03/11/2013 15:54
     // Sistema                  : Rivn
     // Clase para Administrar   : Kilometros de los moviles
     // Basada en la Tabla       : MvlKilometros
@@ -568,6 +568,40 @@ namespace Rivn.Dal
         #endregion
 
         #region Metodos del Usuario
+
+        /// <summary>
+        /// Método Fijo: RemoveAllPatente
+        /// </summary>
+        /// <param name="p_dbcAccess">Conexion a la base de datos</param>
+        /// <param name="p_dsResult">DataSet donde devolver el registro</param>
+        /// <param name="p_strTabla">Nombre de la tabla a llenar</param>
+        /// <param name="p_smResult">Estado final de la operacion</param>
+        public static int RemoveAllPatente(DBConn p_dbcAccess,
+                                           ref DataSet p_dsResult,
+                                           string p_strTabla,
+                                           ref StatMsg p_smResult)
+        {
+            // No hay errores aun
+            p_smResult.DalReset("MvlKilometros", "RemoveAllPatente");
+
+            try {
+                return DBRuts.Exec_DS(p_dbcAccess,
+                                      "TNGS_Rivn..MVLKILOMETROS_REMOVEALLPATENTE",
+                                      new DbParameter[] {
+                                          p_dbcAccess.MakeParam("@usuario", DBConn.Usuario)
+                                      },
+                                      ref p_dsResult, p_strTabla);
+            }
+            catch (Exception l_expData) {
+                // Error en el método fijo
+                p_smResult.DalError(l_expData.ToString());
+                return -1;
+            }
+            finally {
+                // Resteamos el StatMsg
+                p_smResult.DalExit();
+            }
+        }
         #endregion
 
         #region Grillas Definidas
