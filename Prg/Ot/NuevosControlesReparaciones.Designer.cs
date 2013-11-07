71adbe709cd73b2817b11a4cb54f251c4f197e1e
JuanDesi <juandesimoni@gmail.com>
2013-11-06 22:23:10 -0300

    Formulario OT agregado

---
+++ Prg/Ot/NuevosControlesReparaciones.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Rivn.Ot
+{
+    partial class NuevosControlesReparaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xPanel1 = new TNGS.NetControls.XPanel();
+            this.rbReparaciones = new System.Windows.Forms.RadioButton();
+            this.rbControles = new System.Windows.Forms.RadioButton();
+            this.fullLabel1 = new TNGS.NetControls.FullLabel();
+            this.cdCombo1 = new TNGS.NetControls.CDCombo();
+            this.imgGroup1 = new TNGS.NetControls.ImgGroup();
+            this.fullGrid1 = new TNGS.NetControls.FullGrid();
+            this.cdList1 = new TNGS.NetControls.CDList();
+            this.fullLabel2 = new TNGS.NetControls.FullLabel();
+            this.textEdit1 = new TNGS.NetControls.TextEdit();
+            this.xPanel1.SuspendLayout();
+            this.imgGroup1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fullGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xPanel1
+            // 
+            this.xPanel1.BackColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.xPanel1.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(227)))), ((int)(((byte)(242)))));
+            this.xPanel1.Controls.Add(this.textEdit1);
+            this.xPanel1.Controls.Add(this.fullLabel2);
+            this.xPanel1.Controls.Add(this.fullGrid1);
+            this.xPanel1.Controls.Add(this.rbReparaciones);
+            this.xPanel1.Controls.Add(this.rbControles);
+            this.xPanel1.Controls.Add(this.fullLabel1);
+            this.xPanel1.Controls.Add(this.cdCombo1);
+            this.xPanel1.Controls.Add(this.imgGroup1);
+            this.xPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xPanel1.Location = new System.Drawing.Point(0, 0);
+            this.xPanel1.Name = "xPanel1";
+            this.xPanel1.Size = new System.Drawing.Size(1008, 583);
+            this.xPanel1.SkinFixed = true;
+            this.xPanel1.TabIndex = 0;
+            // 
+            // rbReparaciones
+            // 
+            this.rbReparaciones.AutoCheck = false;
+            this.rbReparaciones.AutoSize = true;
+            this.rbReparaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbReparaciones.Location = new System.Drawing.Point(848, 44);
+            this.rbReparaciones.Name = "rbReparaciones";
+            this.rbReparaciones.Size = new System.Drawing.Size(112, 20);
+            this.rbReparaciones.TabIndex = 4;
+            this.rbReparaciones.TabStop = true;
+            this.rbReparaciones.Text = "Reparaciones";
+            this.rbReparaciones.UseVisualStyleBackColor = true;
+            // 
+            // rbControles
+            // 
+            this.rbControles.AutoSize = true;
+            this.rbControles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbControles.Location = new System.Drawing.Point(754, 44);
+            this.rbControles.Name = "rbControles";
+            this.rbControles.Size = new System.Drawing.Size(83, 20);
+            this.rbControles.TabIndex = 3;
+            this.rbControles.TabStop = true;
+            this.rbControles.Text = "Controles";
+            this.rbControles.UseVisualStyleBackColor = true;
+            // 
+            // fullLabel1
+            // 
+            this.fullLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.fullLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullLabel1.Location = new System.Drawing.Point(473, 46);
+            this.fullLabel1.Name = "fullLabel1";
+            this.fullLabel1.Size = new System.Drawing.Size(93, 23);
+            this.fullLabel1.TabIndex = 2;
+            this.fullLabel1.Text = "Movil Patente:";
+            // 
+            // cdCombo1
+            // 
+            this.cdCombo1.BackColor = System.Drawing.SystemColors.Window;
+            this.cdCombo1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cdCombo1.FormattingEnabled = true;
+            this.cdCombo1.Location = new System.Drawing.Point(568, 45);
+            this.cdCombo1.Name = "cdCombo1";
+            this.cdCombo1.Size = new System.Drawing.Size(136, 21);
+            this.cdCombo1.Sorted = true;
+            this.cdCombo1.TabIndex = 1;
+            // 
+            // imgGroup1
+            // 
+            this.imgGroup1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.imgGroup1.BackgroundGradientColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(227)))), ((int)(((byte)(242)))));
+            this.imgGroup1.BackgroundGradientMode = TNGS.NetControls.ImgGroup.GroupBoxGradientMode.ForwardDiagonal;
+            this.imgGroup1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.imgGroup1.BorderThickness = 1F;
+            this.imgGroup1.Controls.Add(this.cdList1);
+            this.imgGroup1.CustomGroupBoxColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(227)))), ((int)(((byte)(242)))));
+            this.imgGroup1.FontTitle = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imgGroup1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.imgGroup1.GroupImage = null;
+            this.imgGroup1.GroupTitle = "Controles y Reparaciones";
+            this.imgGroup1.Location = new System.Drawing.Point(13, 72);
+            this.imgGroup1.Name = "imgGroup1";
+            this.imgGroup1.Padding = new System.Windows.Forms.Padding(20);
+            this.imgGroup1.PaintGroupBox = true;
+            this.imgGroup1.RoundCorners = 10;
+            this.imgGroup1.ShadowColor = System.Drawing.Color.DarkGray;
+            this.imgGroup1.ShadowControl = true;
+            this.imgGroup1.ShadowThickness = 3;
+            this.imgGroup1.Size = new System.Drawing.Size(984, 313);
+            this.imgGroup1.SkinFixed = true;
+            this.imgGroup1.SkinFullFixed = true;
+            this.imgGroup1.TabIndex = 0;
+            // 
+            // fullGrid1
+            // 
+            this.fullGrid1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fullGrid1.DataMember = "";
+            this.fullGrid1.ExcelTitle = "";
+            this.fullGrid1.GridOrder = "";
+            this.fullGrid1.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+            this.fullGrid1.Location = new System.Drawing.Point(13, 393);
+            this.fullGrid1.Name = "fullGrid1";
+            this.fullGrid1.Size = new System.Drawing.Size(983, 178);
+            this.fullGrid1.TabIndex = 0;
+            // 
+            // cdList1
+            // 
+            this.cdList1.FormattingEnabled = true;
+            this.cdList1.Location = new System.Drawing.Point(11, 26);
+            this.cdList1.Name = "cdList1";
+            this.cdList1.Size = new System.Drawing.Size(209, 277);
+            this.cdList1.Sorted = true;
+            this.cdList1.TabIndex = 0;
+            // 
+            // fullLabel2
+            // 
+            this.fullLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.fullLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullLabel2.Location = new System.Drawing.Point(34, 24);
+            this.fullLabel2.Name = "fullLabel2";
+            this.fullLabel2.Size = new System.Drawing.Size(169, 22);
+            this.fullLabel2.TabIndex = 5;
+            this.fullLabel2.Text = "Orden de Trabajo Numero:";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.BackColor = System.Drawing.SystemColors.Window;
+            this.textEdit1.Location = new System.Drawing.Point(206, 23);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(100, 20);
+            this.textEdit1.TabIndex = 6;
+            // 
+            // NuevosControlesReparaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 583);
+            this.Controls.Add(this.xPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "NuevosControlesReparaciones";
+            this.Text = "NuevosControlesReparaciones";
+            this.xPanel1.ResumeLayout(false);
+            this.xPanel1.PerformLayout();
+            this.imgGroup1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fullGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TNGS.NetControls.XPanel xPanel1;
+        private System.Windows.Forms.RadioButton rbReparaciones;
+        private System.Windows.Forms.RadioButton rbControles;
+        private TNGS.NetControls.FullLabel fullLabel1;
+        private TNGS.NetControls.CDCombo cdCombo1;
+        private TNGS.NetControls.ImgGroup imgGroup1;
+        private TNGS.NetControls.FullGrid fullGrid1;
+        private TNGS.NetControls.CDList cdList1;
+        private TNGS.NetControls.TextEdit textEdit1;
+        private TNGS.NetControls.FullLabel fullLabel2;
+    }
+}
---
+++ Prg/Ot/NuevosControlesReparaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TNGS;
+using TNGS.NetControls;
+using TNGS.NetRoutines;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace Rivn.Ot
+{
+    public partial class NuevosControlesReparaciones:DockContent
+    {
+        public NuevosControlesReparaciones()
+        {
+            InitializeComponent();
+        }
+
+
+      
+    }
+}
