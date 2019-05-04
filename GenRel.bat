c6e5ee41972191e73f5aeef101c40cae1267e450
Matt <codejourney.contact@gmail.com>
2019-05-03 23:18:25 -0300

    Gen Rel change

--- GenRel.bat
+++ GenRel.bat
@@ -3,18 +3,11 @@
 if exist "XX - Release" goto NoCreate
 
 md "XX - Release"
-md "XX - Release\Hlp"
 md "XX - Release\Img"
-md "XX - Release\Plg"
 md "XX - Release\Prg"
 
 :NoCreate
 
-rem **
-rem ** Copiamos los elementos del Help
-rem **
-copy Hlp\*.*                                                                       "XX - Release\Hlp"
-copy Hlp\Img\*.*                                                                   "XX - Release\Hlp\Img"
 
 rem **
 rem ** Copiamos las Imágenes
@@ -43,7 +36,6 @@ copy "T:\Mrln0200\ExternalLibraries\Bunifu_UI_v1.52.dll"                        
 rem **
 rem ** Copiamos los elementos del Proyecto: Merlin
 rem **
-REM copy Pin\Def\bin\Release\Rivn.PlgDef.dll                                           "XX - Release\Prg"
 REM copy Pin\<TODO>\bin\Release\Rivn.<TODO>.Plugin.dll                                         "XX - Release\Plg"
 
 copy Dal\bin\Release\Mrln.Dal.dll                                                  "XX - Release\Prg"
@@ -56,11 +48,12 @@ copy Prg\Ot\bin\Release\Mrln.Ot.exe                                             
 copy Prg\Rp\bin\Release\Mrln.Rp.exe                                                "XX - Release\Prg"
 copy Prg\Tg\bin\Release\Mrln.Tg.exe                                                "XX - Release\Prg"
 
+rem ** Armamos la distribucion final para los servidores
+xcopy "Acf" /s /y                                                                "XX - Release\Acf"
+copy "Sql\Install\Mrln SProcs.sql"                                               "XX - Release"
+
 REM echo .
-REM echo Generando EUse
 REM echo .
-REM cd "XX - Release\EUse"
-REM T:\Ambiente0520\@NetExternalTools\ilmerge /internalize:Excludes.txt ..\Prg\Rivn.Shr.dll ..\Prg\Rivn.Bel.dll ..\Prg\Rivn.Bll.dll ..\Prg\Rivn.Dal.dll ..\Prg\Bohman.MonthCalendar.520.dll ..\Prg\MikeHankey.XPControlLib.520.dll ..\Prg\MS.TabStripControlLibrary.520.dll ..\Prg\netchartdir.dll ..\Prg\WeifenLuo.WinFormsUI.Docking.520.dll ..\Prg\TNGS.NetAppBll.520.dll ..\Prg\TNGS.NetControls.520.dll ..\Prg\TNGS.NetOffice.520.dll ..\Prg\TNGS.NetRoutines.520.dll ..\Prg\TNGS.NetSocket.520.dll /out:Rivn.Shr.EUse.dll
 REM cd ..
 REM cd ..
 
